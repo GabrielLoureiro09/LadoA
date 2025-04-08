78a91bd059114bef17609412599670f57c598fae
Gabriel Loureiro <157625824+GabrielLoureiro09@users.noreply.github.com>
2025-04-08 13:20:45 -0300

    Deixando o login responsivo

--- reactladoa/src/Login/Login.js
+++ reactladoa/src/Login/Login.js
@@ -61,35 +61,35 @@ function Container() {
     
     return (
         <div>
-            <div id="container-login"></div>
-            <div id="texto-email-login">Preencha com seu e-mail</div>
-            <form onSubmit={handleSubmit}>
-                <div id="email-box-login">
-                    <div id="email-background-login"></div>
-                    <div id="email-icon-login"></div>
-                    <img id="email-img-login" src={email} alt="Ícone de e-mail" />
-                    <label>
-                        <input id="email-text-login" type="email" ref={emailRef} name="email" placeholder="rafael.cruz@gmail.com" />
-                    </label>
-                    <div id="email-label-login">Insira seu e-mail</div>
-                </div>
+            <div id="container-login">
+                <div id="texto-email-login">Preencha com seu e-mail</div>
+                <form onSubmit={handleSubmit} id="form-login">
+                    <div id="email-box-login">
+                        <div id="email-background-login"></div>
+                        <div id="email-icon-login"></div>
+                        <img id="email-img-login" src={email} alt="Ícone de e-mail" />
+                        <label>
+                            <input id="email-text-login" type="email" ref={emailRef} name="email" placeholder="rafael.cruz@gmail.com" />
+                        </label>
+                        <div id="email-label-login">Insira seu e-mail</div>
+                    </div>
 
-                <div id="senha-box-login">
-                    <div id="senha-background-login"></div>
-                    <div id="senha-icon-login"></div>
-                    <div id="senha-input-background-login"></div>
-                    <label>
-                        <input id="senha-text-login" type="password" ref={senhaRef} name="senha" placeholder="******" />
-                    </label>
-                    <div id="senha-label-login">Insira sua senha</div>
-                    <div id="senha-requisitos-login">A senha deve conter ao menos um caracter especial (! @ # $ % & *) e um tamanho de 8 dígitos</div>
-                    <img id="senha-img-login" src={cadeado} alt="Ícone de senha" />
-                </div>
-
+                    <div id="senha-box-login">
+                        <div id="senha-background-login"></div>
+                        <div id="senha-icon-login"></div>
+                        <div id="senha-input-background-login"></div>
+                        <label>
+                            <input id="senha-text-login" type="password" ref={senhaRef} name="senha" placeholder="******" />
+                        </label>
+                        <div id="senha-label-login">Insira sua senha</div>
+                        <div id="senha-requisitos-login">A senha deve conter ao menos um caracter especial (! @ # $ % & *) e um tamanho de 8 dígitos</div>
+                        <img id="senha-img-login" src={cadeado} alt="Ícone de senha" />
+                    </div>
+                </form>
                 <button id="botao-login" type="submit">
-                    Iniciar sessão
+                        Iniciar sessão
                 </button>
-            </form>
+            </div>
             <br />
             <br />
             <br />
